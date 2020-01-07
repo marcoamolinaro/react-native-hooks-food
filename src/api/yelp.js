9d3cc212cd591abd28a73adae47a3426fc3e5392
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-01-07 16:10:30 -0300

    Updated yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer 8pPm-NKOqu_AYKfEL4MxrbRvNJ2NpGY1SmKt77hjdaZe4zfIdDS6gyuqiDQ5AlZQS7BuRXG6qz6TDundXqmnHF1sJAzev6uBGOW7vM499T1dbXB7oQ2CqwVYMeERXnYx'
+        Authorization: 
+        'Bearer 8pPm-NKOqu_AYKfEL4MxrbRvNJ2NpGY1SmKt77hjdaZe4zfIdDS6gyuqiDQ5AlZQS7BuRXG6qz6TDundXqmnHF1sJAzev6uBGOW7vM499T1dbXB7oQ2CqwVYMeERXnYx'
     }
 });
