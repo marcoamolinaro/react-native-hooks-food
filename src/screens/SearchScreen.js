433cfc8042625b77b0fa655f42519ea3686ffaf9
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-01-11 09:48:07 -0300

    98. Running an Initial Search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -10,12 +10,12 @@ const SearchScreen = () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async () => {
+    const searchApi = async (searchTerm) => {
       try {
         const response = await yelp.get('/search', {
           params: {
             limit: 50,
-            term,
+            term: searchTerm,
             location: 'rio de janeiro'
           }
         });
@@ -26,12 +26,15 @@ const SearchScreen = () => {
 
     };
 
+    // Call searchApi when component is first rendered (BAD CODE!)
+    // searchApi('pasta');
+
     return (
         <View>
             <SearchBar 
                 term={term} 
                 onTermChange={setTerm}
-                onTermSubmit={searchApi}
+                onTermSubmit={searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
