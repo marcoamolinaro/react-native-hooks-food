a93478921d1261d6e510d6528d3377be412a96a1
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-01-12 16:53:28 -0300

    105. Showing Additional Info

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -5,11 +5,25 @@ import ResultList from './ResultList';
 const ResultsDetail = ({ result }) => {
     return (
         <View>
-            <Text>{result.name}</Text>
+            <Image style={styles.imageStyle} source={{ uri: result.image_url }}/>
+            <Text style={styles.nameStyle}>{result.name}</Text>
+            <Text>
+                {result.rating} Stars, {result.review_count} Reviews
+            </Text>
         </View>
     );
 };
 
-const styles=StyleSheet.create({});
+const styles=StyleSheet.create({
+    imageStyle: {
+        width: 200,
+        height: 120,
+        borderRadius: 4
+    },
+    nameStyle: {
+        fontWeight: 'bold',
+        color: 'blue'
+    }
+});
 
 export default ResultsDetail;
