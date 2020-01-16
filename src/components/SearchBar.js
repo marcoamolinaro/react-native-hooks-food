8ba9baf03195a57a3afc4f48f8acd2fef79919fa
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2020-01-15 23:50:17 -0300

    110. Spacing on the Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginHorizontal: 15,
         flexDirection: 'row',
-        alignItems: 'center'
+        marginBottom: 10
     },
     inputStyle: {
         flex: 1,
